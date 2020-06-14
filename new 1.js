09ee180f2206e9121295b65e28e3a8be882fbd20
horasjey <45363813+horasjey@users.noreply.github.com>
2020-06-14 21:55:42 +0700

    Add files via upload

---
+++ new 1.js
@@ -0,0 +1,8 @@
+//---------- Setting Auto Generate -----------
+script_auto_random_post = 'on';
+direct_to_link = 'xiaomi.mrsolusi.com';
+type_direct = 'only'; //all or only
+no_direct_domain ='facebook.com,wa.me';
+only_direct_domain = 'reupload.org,drive.google.com,modsbase.com,drive.mrsolusi.com,racaty.com,gdriveplayer.us,uptobox.com,sharemods.com,filecrypt.co,reupload.org,mirrored.to,fembed.com,acefile.co';
+type_target_click = '_blank'
+path = '#?o=';
